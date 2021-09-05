083ebe257c523a97b5b67c47f8b5809062bd5d81
YevgeniyPivnyak <dneprojek@gmail.com>
2021-09-05 16:10:08 +0300

    implement drop down company description

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
-const xhr = new XMLHttpRequest();
+const text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis illum, porro eligendi nostrum consectetur incidunt rem eius illo. Eligendi beatae culpa quibusdam corrupti ipsum vitae enim, ex in exercitationem incidunt."
 
-xhr.open('GET', 'http://learn.api.axenov-it.com/companies', false);
+fetch('http://learn.api.axenov-it.com/companies')
+    .then(format).then(renderCompanies)
 
-xhr.send();
 
-const companies = JSON.parse(xhr.response)
-
-const content = document.querySelector("#content")
-
-let html = ""
-let counter = 1
-
-for (const company of companies) {
-    html += `<div class="company-row">
-        <div class="number-of-company">${counter++}</div>
-        <h3>${company.name}</h3>
-        <div>${company.address}</div>
-        <div>${company.phones}</div>
-    </div>`
-
+function format(response) {
+    return response.json();
 }
 
-content.innerHTML = html
+function renderCompanies(companies) {
+    const content = document.querySelector("#content")
 
-console.log(html);
+    let counter = 1
+
+    for (const company of companies) {
+        const row = document.createElement("div")
+        row.className = "company-row";
+        row.onclick = (e) => {
+            const elem = e.currentTarget.querySelector(".company-info");
+            if (elem.classList.contains("hide")) {
+                elem.classList.replace("hide", "show");
+            } else {
+                elem.classList.replace("show", "hide");
+            }
+            console.log(elem.classList.contains("hide"))
+        };
+
+        row.innerHTML = `
+            <div class="company-header">
+                <div class="number-of-company">${counter++}</div>
+                <h3>${company.name}</h3>
+                <div>${company.address}</div>
+                <div>${company.city}</div>
+            </div>
+            <div class="company-info hide" ><div class="company-text">${text}</div></div>
+           `
+        content.appendChild(row)
+    }
+
+}
 
