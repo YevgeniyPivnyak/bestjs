c1a20892a20b0940411867a33c179564ea54f802
YevgeniyPivnyak <dneprojek@gmail.com>
2021-09-12 20:39:17 +0300

    OOP style

--- main.js
+++ main.js
@@ -13,46 +13,57 @@ function createCounter(start = 0, step = 1) {
     return () => index += step
 }
 
-const showInfo = (e) => {
-    const elem = e.currentTarget.querySelector(".company-info");
-
-    if (elem.classList.contains("hide")) {
-        elem.classList.replace("hide", "show");
-    } else {
-        elem.classList.replace("show", "hide");
-    }
-};
-
-
-function buildCompany(data, counter) {
-    const { name, address, city } = data
-
-    const row = document.createElement("div")
-    row.className = "company-row";
-
-    row.onclick = showInfo;
-
-    row.innerHTML = `
-        <div class="company-header">
-            <div class="number-of-company">${counter}</div>
-            <h3>${name}</h3>
-            <div>${address}</div>
-            <div>${city}</div>
-        </div>
-        <div class="company-info hide" ><div class="company-text">${LOREN_TEXT}</div></div>
-       `
-    return row
-}
-
-
 function renderCompanies(companies) {
     const content = document.querySelector("#content")
     const counter = createCounter(1);
 
-    for (const company of companies) {
-        const row = buildCompany(company, counter());
+    for (const dataCompany of companies) {
 
-        content.appendChild(row)
+        const company = new Company(dataCompany, counter())
+
+        content.appendChild(company.buildCompany())
+    }
+}
+class Company {
+    constructor(data, counter) {
+        this.data = data
+        this.counter = counter
+    }
+
+    showInfo = (e) => {
+        const elem = e.currentTarget.querySelector(".company-info");
+
+        if (elem.classList.contains("hide")) {
+            elem.classList.replace("hide", "show");
+        } else {
+            elem.classList.replace("show", "hide");
+        }
+    }
+
+    renderTemplate() {
+        const { name, address, city } = this.data
+
+        return `
+            <div class="company-header">
+                <div class="number-of-company">${this.counter}</div>
+                <h3>${name}</h3>
+                <div>${address}</div>
+                <div>${city}</div>
+            </div>
+            <div class="company-info hide" >
+                <div class="company-text">${LOREN_TEXT}</div>
+            </div>
+            `
+    }
+
+    buildCompany() {
+        const company = document.createElement("div")
+        company.className = "company-row";
+
+        company.onclick = this.showInfo;
+
+        company.innerHTML = this.renderTemplate()
+        return company
     }
 
 }
